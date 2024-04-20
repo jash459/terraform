e9b538beb62c13bdba520237c672e00f214ad833
shivakumar139 <shivakumar.pp98@gmail.com>
2024-04-20 13:36:04 +0530

    removed outputs.tf file

--- module.tf
+++ module.tf
@@ -1,18 +1,3 @@
 provider "aws" {
   region = "ap-south-1"
 }
-
-module "ec2_instance" {
-  source = "./module/ec2"
-  instance_name = "ec2_instance"
-}
-
-module "ec2_instance2" {
-  source = "./module/ec2"
-  instance_name = "ec2_instance2"
-}
-
-module "ec2_instance3" {
-  source = "./module/ec2"
-  instance_name = "ec2_instance3"
-}
